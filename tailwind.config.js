16f35951e0922417f32060141ec7768f1b643ebb
Flagster <63302966+ptbandeira@users.noreply.github.com>
2025-09-11 22:22:39 +0000

    Remove conflict markers from tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
-export default {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-=======
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
->>>>>>> 667b294 (Fix build configuration syntax errors)
   theme: {
     extend: {},
   },
